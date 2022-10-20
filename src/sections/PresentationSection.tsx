56de3d954c6ae6b555e9f3276bb6f063225448d1
Cito2310 <cito2310@gmail.com>
2022-10-20 00:04:26 -0300

    Added and styled presentation-section

--- src/sections/PresentationSection.tsx
+++ src/sections/PresentationSection.tsx
@@ -1,5 +1,12 @@
+import "../styles/presentation-section.scss"
+
 export const PresentationSection = () => {
   return (
-    <div className="presentation-section">PresentationSection</div>
+    <section className="presentation-section">
+      <div>
+        <h1>JUAN MEZA</h1>
+        <h2>Desarrollador Front-end y Back-end</h2>
+      </div>
+    </section>
   )
 }
